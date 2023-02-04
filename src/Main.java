01febf001f30fe56f0a7f399e13000e5987b9cfd
markyd-XXI <sandmdelgados@gmail.com>
2023-02-04 12:09:24 -0600

    Inital Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * This program will demonstrate the singleton and adapter patterns by creating a logger. Different clients to the logger may have to adapt
+ * thier interfaces to be able to log stuff out.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
